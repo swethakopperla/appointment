6f2294e3870ac6308ccf9dcade376027ddcfd20c
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2018-01-07 21:36:37 +0530

    updated

--- action.php
+++ action.php
@@ -19,6 +19,28 @@ if(!$conn)
 die("could not connect to the DB".mysqli_connect_error());
 else
 echo "connected sucessfully............. <br>";
+	$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+	$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 $sql="insert into patients(name,email,number,date,department,dname,comment) values('$name','$email','$number','$date','$department','$dname','$text');";
 if(mysqli_query($conn,$sql))
 echo "record sucessfully inserted.......";
